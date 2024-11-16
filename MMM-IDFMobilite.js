7d5fb7efa284613666f463e6f50e7accd803b9f5
Clément Tessier <clement.tessier@ymail.com>
2024-11-16 18:41:34 +0100

    Implement module

---
+++ MMM-IDFMobilite.js
@@ -0,0 +1,127 @@
+
+Module.register("MMM-IDFMobilite", {
+
+  defaults: {
+    exampleContent: "",
+    apiKey: "",
+    title: "",
+    line: "",
+    stop: "",
+    direction: "",
+    maxResults: 3,
+    minGap: 0,
+  },
+
+  // Define required scripts.
+  getScripts() {
+    return ["moment.js"];
+  },
+
+  /**
+   * Apply the default styles.
+   */
+  getStyles() {
+    return ["MMM-IDFMobilite.css"];
+  },
+
+  /**
+   * Pseudo-constructor for our module. Initialize stuff here.
+   */
+  start() {
+    this.departures = [];
+
+    moment.locale(config.language);
+
+    this.fetchNextDepartures();
+    setInterval(() => this.fetchNextDepartures(), 30000);
+  },
+
+  fetchNextDepartures() {
+    this.sendSocketNotification("FETCH_DEPARTURES", {
+      id: this.config.title,
+      line: this.config.line,
+      stop: this.config.stop,
+      destination: this.config.direction,
+      maxResults: this.config.maxResults,
+      minGap: this.config.minGap,
+      apiKey: this.config.apiKey
+    });
+  },
+
+  /**
+   * Handle notifications received by the node helper.
+   * So we can communicate between the node helper and the module.
+   *
+   * @param {string} notification - The notification identifier.
+   * @param {any} payload - The payload data`returned by the node helper.
+   */
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "IDFMOBILITE_EVENT" && payload.id === this.config.title) {
+      switch (payload.type) {
+        case "DEPARTURES_RECEIVED": {
+          this.departures = payload.departures;
+          this.updateDom();
+          break;
+        }
+        case "ERROR": {
+          Log.error(payload.error);
+          break;
+        }
+        default: {
+          Log.warn("Unknown event type received: " + payload);
+        }
+      }
+    }
+  },
+
+  // Override getHeader method.
+  getHeader () {
+    if (this.departures.length > 0) {
+      return `${this.config.title} - ${this.departures[0].destination}`;
+    }
+
+    return this.config.title;
+  },
+
+  /**
+   * Render the page we're on.
+   */
+  getDom() {
+    var table = document.createElement("table");
+
+    // adding next schedules
+    this.departures.forEach((d) => {
+      var row = document.createElement("tr");
+      var timeCell = document.createElement("td");
+      timeCell.innerHTML = moment(d.datetime).format("HH:mm");
+      timeCell.className = "cell time";
+      row.appendChild(timeCell);
+
+      if (d.note) {
+        var noteCell = document.createElement("td");
+        noteCell.innerHTML = d.note;
+        noteCell.className = "cell note";
+        row.appendChild(noteCell);
+      }
+
+      var atStopCell = document.createElement("td");
+      atStopCell.innerHTML = d.atStop ? "🚆" : "";
+      atStopCell.className = "cell at-stop";
+      row.appendChild(atStopCell);
+
+      var statusCell = document.createElement("td");
+      statusCell.innerHTML = d.status === "onTime" ? "🟢" : `🟠  (${d.status})`;
+      statusCell.className = "cell status";
+      row.appendChild(statusCell);
+
+      var releativeTimeCell = document.createElement("td");
+      releativeTimeCell.innerHTML = moment(d.datetime).fromNow();
+      releativeTimeCell.className = "cell relative-time";
+      row.appendChild(releativeTimeCell);
+
+      table.appendChild(row);
+    });
+
+    return table;
+  },
+});
--- node_helper.js
+++ node_helper.js
@@ -1,12 +1,70 @@
 const NodeHelper = require("node_helper")
+const Log = require("logger");
+const moment = require("moment");
 
 module.exports = NodeHelper.create({
+  start() {
+    Log.log(`Starting node helper for: ${this.name}`);
+  },
 
-  async socketNotificationReceived(notification, payload) {
-    if (notification === "GET_RANDOM_TEXT") {
-      const amountCharacters = payload.amountCharacters || 10
-      const randomText = Array.from({ length: amountCharacters }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join("")
-      this.sendSocketNotification("EXAMPLE_NOTIFICATION", { text: randomText })
+  socketNotificationReceived(notification, payload) {
+    if (notification === "FETCH_DEPARTURES") {
+      console.log(payload);
+      this.fetchNextDepartures(payload.id, payload.line, payload.stop, payload.destination, payload.maxResults, payload.minGap, payload.apiKey);
     }
   },
+
+  getApiUrl(line, stop) {
+    return `https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?LineRef=${line}&MonitoringRef=${stop}`
+  },
+
+  getHeaders(apiKey) {
+    return {
+      apiKey,
+    }
+  },
+
+  getDeparturesForDestination(responseData, destination) {
+    return responseData.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit
+      .filter(c => c.MonitoredVehicleJourney.DestinationRef.value === destination)
+  },
+
+  fetchNextDepartures(id, line, stop, destination, maxResults, minGap, apiKey) {
+    fetch(this.getApiUrl(line, stop), { headers: this.getHeaders(apiKey) })
+      .then(response => response.json())
+      .then((responseData) => {
+
+        const data = this.getDeparturesForDestination(responseData, destination);
+        Log.info(`Received ${data.length} departures for ${line} at ${stop} towards ${destination}`);
+        return data.map((d) => {
+          const departure = d.MonitoredVehicleJourney.MonitoredCall
+
+          return {
+            datetime: departure.ExpectedArrivalTime,
+            destination: departure.DestinationDisplay?.[0]?.value,
+            time: moment(departure.ExpectedArrivalTime).format("HH:mm"),
+            relativeTime: moment(departure.ExpectedArrivalTime).fromNow(),
+            atStop: departure.VehicleAtStop,
+            status: departure.DepartureStatus,
+            note: d.MonitoredVehicleJourney.JourneyNote?.[0]?.value,
+          }
+        })
+      })
+      .then((departures) => {
+        this.sendSocketNotification("IDFMOBILITE_EVENT", {
+          id,
+          type: "DEPARTURES_RECEIVED",
+          departures: departures.filter(d => !minGap || moment(d.datetime).isAfter(moment().add(minGap, "minutes"))).slice(0, maxResults),
+        });
+      })
+      .catch((error) => {
+        Log.error(error)
+        this.sendSocketNotification("IDFMOBILITE_EVENT", {
+          type: "ERROR",
+          id,
+          error,
+        })
+      });
+  },
+
 })
